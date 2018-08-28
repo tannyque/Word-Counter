0f16ed6cf0997f3bddf998377b7b6281c42c0239
tannyque <tannyque1992@gmail.com>
2018-08-28 08:06:58 +0100

    Working with input field :closed_book:

--- src/app.js
+++ src/app.js
@@ -1,3 +1,21 @@
+const InputView = require('./views/input_view.js');
+const WordCounter = require('./models/word_counter.js');
+const ResultView = require('./views/result_view.js')
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  // const form = document.querySelector('#wordcounter-form');
+  // form.addEventListener('submit', handleFormSubmit);
+
+  const inputView = new InputView();
+  inputView.bindEvents();
+
+  const wordCounter = new WordCounter();
+  wordCounter.bindEvents();
+
+  const resultView = new ResultView();
+  resultView.bindEvents();
 });
+//
+// const handleFormSubmit = function (event) {
+//   event.preventDefault();
+// };
